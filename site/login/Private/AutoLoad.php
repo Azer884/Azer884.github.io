779d4ad856d50788ad6ef28a9a5a0ab879355203
Azer Hamdaoui <azer.hamdaoui.07@gmail.com>
2024-06-24 23:56:05 +0100

    Revert "Add Php"
    This reverts commit 0b6a4f9b10b5263aeaef4904d0064ec233639ca7.

--- site/login/Private/AutoLoad.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-}
-ini_set("display_errors",0);
-
-require "../private/DataBase.php";
-require "../private/Functions.php";
-?>
